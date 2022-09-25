db1289078eee1c261495edf96a5e580d5c742718
tanisha111 <92454871+tanisha111@users.noreply.github.com>
2022-09-25 21:19:47 +0500

    Add files via upload

---
+++ lec.py
@@ -0,0 +1,19 @@
+#a = 123
+#b = 1.23
+#print (f'{a}, {b}')
+#print ('{1} - {0}'.format(a, b))
+
+#day = int(input'введи число')
+#if day<=7  and day >=1:
+#i = 0
+#size = len(a) # n
+#while i<size:
+ #   if a[i] < 5:
+  #       print(f'{a[i]}')
+   # i += 1
+
+day = int(input('введите день недели '))
+if (day >= 6):
+    print (day, '- выходной ура')
+else:
+    print (day, '- нет')
