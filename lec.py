d694621fe7e4e929665994d8173f9f08a95ac492
tanisha111 <92454871+tanisha111@users.noreply.github.com>
2022-09-25 21:25:32 +0500

    Update lec.py

--- lec.py
+++ lec.py
@@ -1,15 +1,96 @@
-#a = 123
-#b = 1.23
-#print (f'{a}, {b}')
-#print ('{1} - {0}'.format(a, b))
+#1
+day = int(input('введите день недели '))
+if (day >= 6):
+    print (day, '- выходной ура')
+else:
+    print (day, '- нет')
+#2
+def inputNumbers(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
 
-#day = int(input'введи число')
-#if day<=7  and day >=1:
-#i = 0
-#size = len(a) # n
-#while i<size:
- #   if a[i] < 5:
-  #       print(f'{a[i]}')
-   # i += 1
+
+def checkPredicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+
+statement = inputNumbers(3)
+
+if checkPredicate(statement) == True:
+    print(f"Утверждение истинно")
+else:
+    print(f"Утверждение ложно")
+
+
+#3
+def inputKoord(x):
+    a = [0] * x
+    for i in range(x):
+        is_OK = False
+        while not is_OK:
+            try:
+                number = float(input(f"Введите {i+1} координату: "))
+                a[i] = number
+                is_OK = True
+                if a[i] == 0:
+                    is_OK = False
+                    print("Координата не должно быть равна 0 ")
+            except ValueError:
+                print("Ты ошибся. Вводить надо числа!")
+    return a
+
+
+def checkCoordinates(xy):
+    text = 4
+    if xy[0] > 0 and xy[1] > 0:
+        text = 1
+    elif xy[0] < 0 and xy[1] > 0:
+        text = 2
+    elif xy[0] < 0 and xy[1] < 0:
+        text = 3
+    print(f"Точка находится в {text} четверти плоскости")
+
+
+koordinate = inputKoord(2)
+checkCoordinates(koordinate)
+
+#4
+
+def inputNumbers(x):
+    xy = ["X", "Y"]
+    a = []
+    for i in range(x):
+        is_OK = False
+        while not is_OK:
+            try:
+                number = int(input(f"Введите координату по {xy[i]}: "))
+                a.append(number)
+                is_OK = True
+            except ValueError:
+                print("Ты ошибся. Вводить надо целые числа!")
+    return a
+
+
+def calculateLengthSegment(a, b):
+    lengthSegment = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** (0.5)
+    return lengthSegment
+
+
+print("Введите координаты точки А")
+pointA = inputNumbers(2)
+print("Введите координаты точки В")
+pointB = inputNumbers(2)
+
+print(f"Длина отрезка: {format(calculateLengthSegment(pointA, pointB), '.2f')}")
+
+
+
+
 
 
